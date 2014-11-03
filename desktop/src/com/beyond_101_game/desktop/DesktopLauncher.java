79f2e3ada4082cd51bcb09497c40100cdc5c310c
Rik Norakomi <rikvanvelzen@yahoo.com>
2014-11-03 13:56:14 +0100

    Bareboen project commit
    First one!

---
+++ desktop/src/com/beyond_101_game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.beyond_101_game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.beyond_101_game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
